5be7529918b6cdf769282561429df61630a941ac
Anna Mikhaylova <anna@Andreys-MacBook-Pro.local>
2019-12-21 22:51:43 +0300

    create project, webpack, babel, eslint, ts

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import '../styles/index.css';
+
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <h1>Анечка, поставь Redux!!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
